b431937cabd3bbe5669dc18fabda96688ffe2236
sadia <sadia.nury@gmail.com>
2024-04-25 02:54:57 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/destini_challange_starting1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.destini_challange_starting1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/story.dart
@@ -0,0 +1,7 @@
+class Story {
+  String choice1;
+  String choice2;
+  String  storyTitle;
+
+  Story({required this.choice1,required this.choice2,required this.storyTitle});
+}
